6a9582a8f30df8fbed16460ebe20917b5d53187e
Mark <markriaz@mail.ru>
2016-09-06 22:49:51 +0200

    acceptance_helper

--- spec/acceptance/add_and_see_all_answers.rb
+++ spec/acceptance/add_and_see_all_answers.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'acceptance_helper'
 
 feature 'user add answer', %q{
   user can see question
--- spec/acceptance/ask_and_see_all_question.rb
+++ spec/acceptance/ask_and_see_all_question.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'acceptance_helper'
 
 feature 'user ask question', %q{
   to get answer
